8b46114c5ebbe4901ce9c920af9232fae26170c1
Alexey Khodyrev <akhodyrev@sportmaster.ru>
2019-11-18 08:18:23 +0300

    format code

--- sunbox/src/main/java/ru/cwark/p2t/sunbox/MyFirstProgram.java
+++ sunbox/src/main/java/ru/cwark/p2t/sunbox/MyFirstProgram.java
@@ -1,9 +1,9 @@
 package ru.cwark.p2t.sunbox;
 
 public class MyFirstProgram {
-	
-	public static void main (String[] args){
-		System.out.println("Hello, world!");
-	}
-	
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
 }
