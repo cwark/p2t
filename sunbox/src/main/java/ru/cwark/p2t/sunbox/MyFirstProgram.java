c994d5c2035f7159a5638a8031a3eb70c1292464
Alexey Khodyrev <akhodyrev@sportmaster.ru>
2019-11-19 04:56:18 +0300

    добавлены функции

--- sunbox/src/main/java/ru/cwark/p2t/sunbox/MyFirstProgram.java
+++ sunbox/src/main/java/ru/cwark/p2t/sunbox/MyFirstProgram.java
@@ -3,7 +3,32 @@ package ru.cwark.p2t.sunbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("Lesha");
+    hello("user");
+    hello();
+
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello() {
     System.out.println("Hello, world!");
+  }
+
+  public static void hello(String name) {
+    System.out.println("Hello, " + name + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
