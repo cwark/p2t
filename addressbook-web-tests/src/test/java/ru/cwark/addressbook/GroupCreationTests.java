7bc777d7cb7258c07291c7332a9fcb484c7a0cf6
Alexey Khodyrev <ahodyrev@mail.ru>
2019-11-23 15:46:44 +0300

    удалил мёртвый код

--- addressbook-web-tests/src/test/java/ru/cwark/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/cwark/addressbook/GroupCreationTests.java
@@ -47,23 +47,5 @@ public class GroupCreationTests {
     wd.quit();
   }
 
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
 }
 
