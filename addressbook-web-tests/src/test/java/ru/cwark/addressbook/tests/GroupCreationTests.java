8a88396db2aab05bf0eca12e3c2fe80034a46037
Alexey Khodyrev <ahodyrev@mail.ru>
2019-12-05 00:52:40 +0300

    add CreateGroup

--- addressbook-web-tests/src/test/java/ru/cwark/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/cwark/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("group3", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("group3", null, null));
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/cwark/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/cwark/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.cwark.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.cwark.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("group4", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnGroupPage();
